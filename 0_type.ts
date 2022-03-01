533a9eba461ab7a60edb118ed509018b414683d2
luc-dev10 <luc.dev1008@gmail.com>
2022-02-28 22:35:31 -0500

    add types

---
+++ 0_type.ts
@@ -0,0 +1,20 @@
+// strings
+let firstName: String = "lucio";
+
+// numbers
+const age: Number = 123;
+
+// any type
+let anyType: any = 123;
+anyType = "Hello";
+
+// boolean
+let isTrue: boolean = true;
+
+// null
+let nullValue: null = null;
+
+// undefined
+let undefinedValue: undefined = undefined;
+
+console.log(firstName, age, anyType, isTrue, nullValue, undefinedValue);
